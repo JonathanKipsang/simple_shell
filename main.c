be43ad4f2ff49e4326d58e74c9741ffb6d821dd8
Jude Alistair <jude55593@gmail.com>
2023-04-25 23:36:02 -0700

    shell_updated

--- main.c
+++ main.c
@@ -1,41 +1,39 @@
 #include "shell.h"
+
 /**
-* main - entry point
-* @ac: arg count
-* @av: arg vector
-*
-* Return: 0 on success, 1 on error
-*/
-int main(int ac, char **av)
+ * main - Simple Shell entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-info_t info[] = { INFO_INIT };
-int fd = 2;
-asm ("mov %1, %0\n\t"
-"add $3, %0"
-: "=r" (fd)
-: "r" (fd));
-if (ac == 2)
-{
-fd = open(av[1], O_RDONLY);
-if (fd == -1)
-{
-if (errno == EACCES)
-exit(126);
-if (errno == ENOENT)
-{
-_eputs(av[0]);
-_eputs(": 0: Can't open ");
-_eputs(av[1]);
-_eputchar('\n');
-_eputchar(BUF_FLUSH);
-exit(127);
-}
-return (EXIT_FAILURE);
-}
-info->readfd = fd;
-}
-populate_env_list(info);
-read_history(info);
-hsh(info, av);
-return (EXIT_SUCCESS);
+	char *command = NULL;
+	size_t command_len = 0;
+	ssize_t chars_read;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
+
+		chars_read = getline(&command, &command_len, stdin);
+		if (chars_read == -1)
+			break;
+
+		if (my_strcmp(command, "exit\n") == 0)
+			exit_shell();
+
+		if (execute_command(command) == -1)
+			perror("Error executing command");
+
+		free(command);
+		command = NULL;
+		command_len = 0;
+	}
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+
+	free(command);
+	return (0);
 }
